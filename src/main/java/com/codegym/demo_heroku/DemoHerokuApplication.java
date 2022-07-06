c478f06b22acf9dca697adba4d5718ec7f882486
haitan2408 <haitan28102408@gmail.com>
2022-07-06 11:07:36 +0700

    first commit

---
+++ src/main/java/com/codegym/demo_heroku/DemoHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.demo_heroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHerokuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHerokuApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/codegym/demo_heroku/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.codegym.demo_heroku.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("")
+    public String helloWord() {
+        return "index";
+    }
+}
